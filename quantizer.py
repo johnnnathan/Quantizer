6e39a110dde1f2745501abebd0a580a1d20bc3e9
johnnnathan <johnnnathan@proton.me>
2024-10-23 22:06:16 +0200

    Minor advancement

--- quantizer.py
+++ quantizer.py
@@ -4,27 +4,36 @@ import random
 import math
 
 
+# image array operations
 img = Image.open('image.jpg')
 img_array = np.array(img)
-
-
 img_array = img_array.reshape(-1 , 3)
 
 
+# Value Initialization and Declaration
 random_centroids = []
-
+k = 16
 pixel_count = img_array.shape[0];
-print(pixel_count)
-for i in range(16):
+
+
+# Random Centroid Initialization
+for i in range(k):
     random_number = random.randint(0, pixel_count)
     random_centroids.append(img_array[random_number])
 
+for counter in range(5):
+    random_centroids = np.array(random_centroids)
+    distances = np.linalg.norm(img_array[:, np.newaxis] - random_centroids, axis=2)
 
-random_centroids = np.array(random_centroids)
+    cluster_indexes = np.argmin(distances, axis=1)
 
+    new_centroids = np.zeros((k, 3))
 
-distances = np.linalg.norm(img_array[:, np.newaxis] - random_centroids, axis=2)
+    for i in range(k):
+        cluster_pixels = img_array[cluster_indexes == i]
+        if len(cluster_pixels) != 0:
+            new_centroids[i] = np.mean(cluster_pixels, axis=0)
 
-cluster_indixes = np.argmin(distances, axis=1)
+    random_centroids = new_centroids
 
-print(cluster_indixes)
+print(random_centroids)
