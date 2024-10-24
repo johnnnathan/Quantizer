191afdd79ce5fc42419ee290afc5a6e20edb3600
Dimi <johnnnathan@proton.me>
2024-10-24 20:27:07 +0200

    Good version of quantizer model, probably won't do much more work on it

--- quantizer.py
+++ quantizer.py
@@ -3,37 +3,42 @@ from PIL import Image
 import random
 import math
 
+k = 16
+convergence_threshold = 1e-4
+max_iterations = 5
+converged = False
+cluster_indexes = None
+counter = 0
 
-# image array operations
+random.seed(42)
+np.random.seed(42)
+
 img = Image.open('image.jpg')
 img_array = np.array(img)
-img_array = img_array.reshape(-1 , 3)
+img_array = img_array.reshape(-1, 3)
 
+random_centroids = img_array[np.random.choice(img_array.shape[0], k, replace=False)]
 
-# Value Initialization and Declaration
-random_centroids = []
-k = 16
-pixel_count = img_array.shape[0];
-
-
-# Random Centroid Initialization
-for i in range(k):
-    random_number = random.randint(0, pixel_count)
-    random_centroids.append(img_array[random_number])
-
-for counter in range(5):
-    random_centroids = np.array(random_centroids)
+while not converged and counter < max_iterations:
     distances = np.linalg.norm(img_array[:, np.newaxis] - random_centroids, axis=2)
-
+    
     cluster_indexes = np.argmin(distances, axis=1)
-
-    new_centroids = np.zeros((k, 3))
-
+    
+    old_centroids = random_centroids.copy()
+    
     for i in range(k):
-        cluster_pixels = img_array[cluster_indexes == i]
-        if len(cluster_pixels) != 0:
-            new_centroids[i] = np.mean(cluster_pixels, axis=0)
+        if np.any(cluster_indexes == i):
+            random_centroids[i] = img_array[cluster_indexes == i].mean(axis=0)
+    
+    converged = np.all(np.linalg.norm(old_centroids - random_centroids, axis=1) < convergence_threshold)
+    
+    print(f"Iteration: {counter + 1}")
+    
+    counter += 1
 
-    random_centroids = new_centroids
+quantized_image = random_centroids[cluster_indexes].astype(np.uint8)
+quantized_image = quantized_image.reshape(img.size[1], img.size[0], 3)
 
-print(random_centroids)
+Image.fromarray(quantized_image).save("quantized_image.jpg")
+
+print("Success: Output file generated!")
