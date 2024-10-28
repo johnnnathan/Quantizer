02a34477014dc01cfd282313ade409f0db778359
johnnnathan <johnnnathan@proton.me>
2024-10-28 08:57:25 +0100

    Small fix that caused the images to have more colours than the specified k

--- quantizer.py
+++ quantizer.py
@@ -3,12 +3,26 @@ from PIL import Image
 import random
 import math
 
-k = 16
+k = 64 
 convergence_threshold = 1e-4
-max_iterations = 5
+max_iterations = 1
 converged = False
 cluster_indexes = None
 counter = 0
+
+def get_colour_count(path):
+    image = Image.open(path)
+    colour_counts = {}
+
+    pixels = list(image.getdata())
+
+    for pixel in pixels:
+        if pixel in colour_counts:
+            colour_counts[pixel] += 1
+        else:
+            colour_counts[pixel] = 1
+    print("Amount of colours :" + str(len(colour_counts)))
+
 
 random.seed(42)
 np.random.seed(42)
@@ -16,6 +30,8 @@ np.random.seed(42)
 img = Image.open('image.jpg')
 img_array = np.array(img)
 img_array = img_array.reshape(-1, 3)
+
+get_colour_count('image.jpg')
 
 random_centroids = img_array[np.random.choice(img_array.shape[0], k, replace=False)]
 
@@ -39,6 +55,10 @@ while not converged and counter < max_iterations:
 quantized_image = random_centroids[cluster_indexes].astype(np.uint8)
 quantized_image = quantized_image.reshape(img.size[1], img.size[0], 3)
 
-Image.fromarray(quantized_image).save("quantized_image.jpg")
+
+Image.fromarray(quantized_image).save("quantized_image.png", format="PNG")
 
 print("Success: Output file generated!")
+
+# Verify color count in quantized image
+get_colour_count('quantized_image.png')
